cd88872b83b641b3fd244ff5cbe0ce761682e2b4
Kd125 <kashdangar1250@gmail.com>
2023-03-01 17:06:58 -0600

    Completed API App

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
-let weatherData = [];
-
 const getWeather = () => {
-  fetch(
-    "https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}"
-  )
+  const city = document.getElementById("city-input").value;
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=f10265ad8eb5cb3fb062771b2c175276`;
+  fetch(url)
     .then((res) => res.json())
-    .then((weatherInfo) => (weatherData = weatherInfo));
+    .then((data) => {
+      displayWeather(data);
+    });
+};
+
+const displayWeather = (weather) => {
+  let tempDisplay = document.getElementById("temperature");
+  let descriptionDisplay = document.getElementById("description");
+  tempDisplay.innerHTML = weather.main.temp;
+  descriptionDisplay.innerHTML = weather.weather[0].description;
 };
